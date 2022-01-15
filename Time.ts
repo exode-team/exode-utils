e540fa1ff976e086154c4ed405c3483ddc3d5dd3
Aslan Orlov <aslanorlov@gmail.com>
2022-01-15 19:16:33 +0200

    Reformatting

--- Time.ts
+++ Time.ts
@@ -4,6 +4,9 @@
  * @author: exode <hello@exode.ru>
  */
 
+import moment from 'moment';
+
+
 class Time {
 
     /**
@@ -15,6 +18,30 @@ class Time {
         return new Promise<boolean>(resolve => {
             setTimeout(() => resolve(true), ms);
         });
+    }
+
+    /**
+     * Parse date by format
+     * @param {Date} date
+     * @param {string} format
+     * @returns {string}
+     */
+    static parseDate(date: Date, format: string = 'D MMM YYYY HH:mm'): string {
+        return moment(date, moment.ISO_8601).format(format);
+    }
+
+    /**
+     * Get left dates with dates
+     * @param to
+     * @param {Date} from
+     * @returns {number}
+     */
+    static getDaysLeft(to: Date, from?: Date): number {
+        const source = moment(from || new Date());
+
+        const target = moment(to);
+
+        return target.diff(source, 'days');
     }
 
 }
