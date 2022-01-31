5c482d1be90468540f078769fd47d9a2883ba220
Aslan Orlov <aslanorlov@gmail.com>
2022-01-31 15:37:22 +0200

    Reformatting

--- index.ts
+++ index.ts
@@ -4,13 +4,11 @@
  * @author: exode <hello@exode.ru>
  */
 
-export * from './Component';
 export * from './DbJson';
 export * from './Number';
 export * from './Object';
 export * from './Parse';
 export * from './Http';
-export * from './Scroll';
 export * from './String';
 export * from './Time';
 export * from './Url';
