180e42473035ef8d374593931cadcc743ad8f9e7
Aslan Orlov <aslanorlov@gmail.com>
2022-03-19 08:32:44 +0200

    Reformatting

--- Number.ts
+++ Number.ts
@@ -16,6 +16,30 @@ class NumberUtil {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, delimiter);
     }
 
+    /**
+     * Collapse range with dash
+     * @returns {Array<string>}
+     * @param array
+     */
+    static getRanges(array: number[]): string[] {
+        const ranges = [];
+        let start, rend;
+
+        for (let i = 0; i < array.length; i++) {
+            start = array[i];
+            rend = start;
+
+            while (array[i + 1] - array[i] == 1) {
+                rend = array[i + 1];
+                i++;
+            }
+
+            ranges.push(start == rend ? start + '' : start + '-' + rend);
+        }
+
+        return ranges;
+    }
+
 }
 
 
